49b9dbaba204e6a69465e422f76debac540e75b7
Dimitri Sokolyuk <demon@dim13.org>
2017-12-13 00:37:09 +0100

    ...

--- cmd/elegooctl/main.go
+++ cmd/elegooctl/main.go
@@ -8,17 +8,11 @@ import (
 )
 
 func main() {
-	conf := &serial.Config{
-		//Name: "/dev/tty.usbmodem1421",
-		//Name: "/dev/tty.usbmodem1411",
-		Name: "/dev/tty.Elegoo-DevB",
-		Baud: 57600,
-	}
+	conf := &serial.Config{Name: "/dev/tty.Elegoo-DevB", Baud: 57600}
 	s, err := serial.OpenPort(conf)
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer s.Close()
-
 	elegoo.NewFSM(s).Start()
 }

26098059775d947b25acafbb33160fc2e523a761
Dimitri Sokolyuk <demon@dim13.org>
2018-09-12 13:19:39 +0200

    ...

--- forth/main.fs
+++ forth/main.fs
@@ -2,30 +2,30 @@
 marker -io \ define ports
 
 $23 constant pinb
-$24 constant ddrb
+$24 constant ddb
 $25 constant portb
 
 $26 constant pinc
-$27 constant ddrc
+$27 constant ddc
 $28 constant portc
 
 $29 constant pind
-$2a constant ddrd
+$2a constant ddd
 $2b constant portd
 
 : bv ( bit -- mask ) 1 swap lshift ;
 
 #5 bv portb 2constant led
-#5 bv ddrb mset
+#5 bv ddb mset
 
 #2 bv pind 2constant sr
-#2 bv ddrd mclr
+#2 bv ddd mclr
 
 #4 bv pind 2constant sc
-#4 bv ddrd mclr
+#4 bv ddd mclr
 
 #3 bv pind 2constant sl
-#3 bv ddrd mclr
+#3 bv ddd mclr
 
 : read ( mask port -- flag )
   c@ invert 0= swap drop ;
